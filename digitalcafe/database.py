ef9b813645b08c8021e47ecceda29fb1b17806d7
jazmine-munoz <123607939+jazmine-munoz@users.noreply.github.com>
2023-04-14 02:17:57 +0800

    Update database.py

--- digitalcafe/database.py
+++ digitalcafe/database.py
@@ -6,16 +6,6 @@ products_db = myclient["products"]
 
 order_management_db = myclient["order_management"]
 
-products = {
-    100: {"name":"Americano","price":125},
-    200: {"name":"Brewed Coffee","price":100},
-    300: {"name":"Cappuccino","price":120},
-    400: {"name":"Espresso","price":120},
-    1000:{"name":"Tiramisu","price":150},
-    1100:{"name":"Red Velvet","price":130},
-    1200:{"name":"Mango Cream Pie","price":200},
-}
-
 branches = {
     1: {"name":"Katipunan","phonenumber":"09179990000"},
     2: {"name":"Tomas Morato","phonenumber":"09179990001"},
@@ -25,15 +15,19 @@ branches = {
 }
 
 def get_product(code):
-    return products[code]
+    products_coll = products_db["products"]
+
+    product = products_coll.find_one({"code":code})
+
+    return product
 
 def get_products():
     product_list = []
 
-    for i,v in products.items():
-        product = v
-        product.setdefault("code",i)
-        product_list.append(product)
+    products_coll = products_db["products"]
+
+    for p in products_coll.find({}):
+        product_list.append(p)
 
     return product_list
 
