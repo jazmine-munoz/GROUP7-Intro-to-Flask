0346eb63e5deaf537ee8cb6baa1f5a8f46e79593
jazmine-munoz <123607939+jazmine-munoz@users.noreply.github.com>
2023-04-13 20:55:10 +0800

    Update database.py

--- digitalcafe/database.py
+++ digitalcafe/database.py
@@ -24,30 +24,6 @@ branches = {
     5: {"name":"Arcovia","phonenumber":"09179990004"},
 }
 
-users = {
-    "chums@example.com":{"password":"Ch@ng3m3!",
-                         "first_name":"Matthew",
-                         "last_name":"Uy"},
-    "joben@example.com":{"password":"Ch@ng3m3!",
-                         "first_name":"Joben",
-                         "last_name":"Ilagan"},
-    "bong@example.com":{"password":"Ch@ng3m3!",
-                        "first_name":"Bong",
-                        "last_name":"Olpoc"},
-    "joaqs@example.com":{"password":"Ch@ng3m3!",
-                         "first_name":"Joaqs",
-                         "last_name":"Gonzales"},
-    "gihoe@example.com":{"password":"Ch@ng3m3!",
-                         "first_name":"Gio",
-                         "last_name":"Hernandez"},
-    "vic@example.com":{"password":"Ch@ng3m3!",
-                       "first_name":"Vic",
-                       "last_name":"Reventar"},
-    "joe@example.com":{"password":"Ch@ng3m3!",
-                       "first_name":"Joe",
-                       "last_name":"Ilagan"},
-}
-
 def get_product(code):
     return products[code]
 
